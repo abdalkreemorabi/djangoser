bcd0e67d6f9b7a626f6929fffdafcadb2d653f3c
abdalkreemorabi <abdalkreemorabi11@gmail.com>
2020-11-06 19:49:53 +0200

    Modify DB

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -80,11 +80,9 @@ WSGI_APPLICATION = 'todo_drf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'djongo',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
